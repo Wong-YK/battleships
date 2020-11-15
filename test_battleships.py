87bce2e9b6f5c9d4d12cbafacaf79544b8ace5d3
Wong-YK <59433231+Wong-YK@users.noreply.github.com>
2020-11-15 12:18:46 +0000

    test_battleships.py 15/11 1

--- test_battleships.py
+++ test_battleships.py
@@ -8,18 +8,53 @@ def test_is_sunk1():
 
 def test_ship_type1():
     #add at least one test for ship_type by the deadline of session 7 assignment
+    s = (2, 3, False, 3, {(2,3), (3,3), (4,3)})
+    assert type(shiptype(s))==str
     #provide at least five tests in total for ship_type by the project submission deadline
 
 def test_is_open_sea1():
     #add at least one test for open_sea by the deadline of session 7 assignment
+    f = [(0, 0, True, 4, {}),
+         (2, 0, True, 3, {}),
+         (2, 4, True, 3, {}),
+         (4, 0, True, 2, {}),
+         (4, 3, True, 2, {},
+         (4, 6, True, 2, {}),
+         (6, 0, True, 1, {}),
+         (6, 2, True, 1, {}),
+         (6, 4, True, 1, {}),
+         (6, 6, True, 1, {})]
+    assert type(is_open_sea(1, 1, f))==bool
     #provide at least five tests in total for open_sea by the project submission deadline
 
 def test_ok_to_place_ship_at1():
     #add at least one test for ok_to_place_ship_at by the deadline of session 7 assignment
+    f = [(0, 0, True, 4, {}),
+         (2, 0, True, 3, {}),
+         (2, 4, True, 3, {}),
+         (4, 0, True, 2, {}),
+         (4, 3, True, 2, {},
+         (4, 6, True, 2, {}),
+         (6, 0, True, 1, {}),
+         (6, 2, True, 1, {}),
+         (6, 4, True, 1, {}),
+         (6, 6, True, 1, {})]
+    assert type(ok_to_place_ship_at(0, 0, True, 4, f))==bool
     #provide at least five tests in total for ok_to_place_ship_at by the project submission deadline
 
 def test_place_ship_at1():
     #add at least one test for place_ship_at by the deadline of session 7 assignment
+    f = [(0, 0, True, 4, {}),
+         (2, 0, True, 3, {}),
+         (2, 4, True, 3, {}),
+         (4, 0, True, 2, {}),
+         (4, 3, True, 2, {},
+         (4, 6, True, 2, {}),
+         (6, 0, True, 1, {}),
+         (6, 2, True, 1, {}),
+         (6, 4, True, 1, {}),
+         (6, 6, True, 1, {})]
+    assert type(place_ship_at(0, 0, True, 1, f))==list
     #provide at least five tests in total for place_ship_at by the project submission deadline
 
 def test_check_if_hits1():
