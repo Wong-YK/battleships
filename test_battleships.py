2bd33ee3d55b445dc98e0984313af6334b26ec7d
Wong-YK <59433231+Wong-YK@users.noreply.github.com>
2020-11-15 12:43:06 +0000

    test_battleships.py 15/11 2

--- test_battleships.py
+++ test_battleships.py
@@ -18,7 +18,7 @@ def test_is_open_sea1():
          (2, 0, True, 3, {}),
          (2, 4, True, 3, {}),
          (4, 0, True, 2, {}),
-         (4, 3, True, 2, {},
+         (4, 3, True, 2, {}),
          (4, 6, True, 2, {}),
          (6, 0, True, 1, {}),
          (6, 2, True, 1, {}),
@@ -33,7 +33,7 @@ def test_ok_to_place_ship_at1():
          (2, 0, True, 3, {}),
          (2, 4, True, 3, {}),
          (4, 0, True, 2, {}),
-         (4, 3, True, 2, {},
+         (4, 3, True, 2, {}),
          (4, 6, True, 2, {}),
          (6, 0, True, 1, {}),
          (6, 2, True, 1, {}),
@@ -48,24 +48,56 @@ def test_place_ship_at1():
          (2, 0, True, 3, {}),
          (2, 4, True, 3, {}),
          (4, 0, True, 2, {}),
-         (4, 3, True, 2, {},
+         (4, 3, True, 2, {}),
          (4, 6, True, 2, {}),
          (6, 0, True, 1, {}),
          (6, 2, True, 1, {}),
-         (6, 4, True, 1, {}),
-         (6, 6, True, 1, {})]
+         (6, 4, True, 1, {})]
     assert type(place_ship_at(0, 0, True, 1, f))==list
     #provide at least five tests in total for place_ship_at by the project submission deadline
 
 def test_check_if_hits1():
     #add at least one test for check_if_hits by the deadline of session 7 assignment
+    f = [(0, 0, True, 4, {}),
+         (2, 0, True, 3, {}),
+         (2, 4, True, 3, {}),
+         (4, 0, True, 2, {}),
+         (4, 3, True, 2, {}),
+         (4, 6, True, 2, {}),
+         (6, 0, True, 1, {}),
+         (6, 2, True, 1, {}),
+         (6, 4, True, 1, {}),
+         (6, 4, True, 1, {})]
+    assert type(check_if_hits(0, 0, f))==bool
     #provide at least five tests in total for check_if_hits by the project submission deadline
 
 def test_hit1():
     #add at least one test for hit by the deadline of session 7 assignment
+    f = [(0, 0, True, 4, {}),
+         (2, 0, True, 3, {}),
+         (2, 4, True, 3, {}),
+         (4, 0, True, 2, {}),
+         (4, 3, True, 2, {}),
+         (4, 6, True, 2, {}),
+         (6, 0, True, 1, {}),
+         (6, 2, True, 1, {}),
+         (6, 4, True, 1, {}),
+         (6, 4, True, 1, {})]
+    assert type(hit(0, 0, f))==tuple
     #provide at least five tests in total for hit by the project submission deadline
 
 def test_are_unsunk_ships_left1():
     #add at least one test for are_unsunk_ships_left by the deadline of session 7 assignment
+    f = [(0, 0, True, 4, {}),
+         (2, 0, True, 3, {}),
+         (2, 4, True, 3, {}),
+         (4, 0, True, 2, {}),
+         (4, 3, True, 2, {}),
+         (4, 6, True, 2, {}),
+         (6, 0, True, 1, {}),
+         (6, 2, True, 1, {}),
+         (6, 4, True, 1, {}),
+         (6, 4, True, 1, {})]
+    assert type(are_unsunk_ships_left(f))==bool
     #provide at least five tests in total for are_unsunk_ships_left by the project submission deadline
     
