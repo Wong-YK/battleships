3b55d65b0c9f65971ab12c54f2ca625185deac01
Wong-YK <59433231+Wong-YK@users.noreply.github.com>
2020-11-16 20:58:30 +0000

    changes to test_is_sunk1() and addition of test_is_open_sea2()

--- test_battleships.py
+++ test_battleships.py
@@ -3,13 +3,13 @@ from battleships import *
 
 def test_is_sunk1():
     s = (2, 3, False, 3, {(2,3), (3,3), (4,3)})
-    assert is_sunk(s)
+    assert is_sunk(s)==True
     #add at least four more tests for is_sunk by the project submission deadline
 
 def test_ship_type1():
     #add at least one test for ship_type by the deadline of session 7 assignment
     s = (2, 3, False, 3, {(2,3), (3,3), (4,3)})
-    assert shiptype(s)=="cruiser"
+    assert ship_type(s)=="cruiser"
     #provide at least five tests in total for ship_type by the project submission deadline
 
 def test_is_open_sea1():
@@ -24,8 +24,20 @@ def test_is_open_sea1():
          (6, 2, True, 1, {}),
          (6, 4, True, 1, {}),
          (6, 6, True, 1, {})]
-    assert is_open_sea(0, 0, f)==False
-    #provide at least five tests in total for open_sea by the project submission deadline
+    assert is_open_sea(6, 6, f)==False
+
+def test_is_open_sea2():
+    f = [(0, 0, True, 4, {}),
+         (2, 0, True, 3, {}),
+         (2, 4, True, 3, {}),
+         (4, 0, True, 2, {}),
+         (4, 3, True, 2, {}),
+         (4, 6, True, 2, {}),
+         (6, 0, True, 1, {}),
+         (6, 2, True, 1, {}),
+         (6, 4, True, 1, {}),
+         (6, 6, True, 1, {})]
+    assert (is_open_sea(4, 0, f) and is_open_sea(4, 1, f))==False
 
 def test_ok_to_place_ship_at1():
     #add at least one test for ok_to_place_ship_at by the deadline of session 7 assignment
