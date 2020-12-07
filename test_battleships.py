196bbb62989e5c651f009fd7282efd093e11c83c
Wong-YK <59433231+Wong-YK@users.noreply.github.com>
2020-12-07 18:44:42 +0000

    changes made to tests for ok_to_place_ship_at()

--- test_battleships.py
+++ test_battleships.py
@@ -26,6 +26,7 @@ def test_is_open_sea1():
     assert is_open_sea(6, 6, f)==False
 
 def test_is_open_sea2():
+    #occupied square (top left square of ship)
     f = [(0, 0, True, 4, {}),
          (2, 0, True, 3, {}),
          (2, 4, True, 3, {}),
@@ -36,56 +37,49 @@ def test_is_open_sea2():
          (6, 2, True, 1, {}),
          (6, 4, True, 1, {}),
          (6, 6, True, 1, {})]
-    assert (is_open_sea(4, 0, f) or is_open_sea(4, 1, f))==False
+    assert is_open_sea(4, 0, f)==False
 
 def test_is_open_sea3():
+    #vertically adjacent square
     f = [(6, 0, False, 4, {}),
-         (7, 2, True, 3, {}),
-         (3, 2, True, 3, {}),
-         (8, 4, True, 2, {}),
-         (5, 4, True, 2, {}),
-         (2, 4, True, 2, {}),
-         (2, 9, True, 1, {}),
-         (6, 7, True, 1, {}),
-         (6, 5, True, 1, {}),
-         (6, 3, True, 1, {})]
-    assert (is_open_sea(3, 2, f) or is_open_sea(4, 2, f) or is_open_sea(5, 2, f))==False
+         (1, 0, False, 2, {}),
+         (7, 2, True, 1, {}),
+         (1, 3, True, 1, {}),
+         (5, 3, True, 3, {}),
+         (7, 4, True, 3, {}),
+         (6, 1, False, 2, {}),
+         (4, 7, True, 1, {}),
+         (9, 8, True, 1, {}),
+         (0, 9, False, 2, {})]
+    assert is_open_sea(3, 0, f)==False
 
 def test_is_open_sea4():
-    f = [(0, 0, True, 4, {}),
-         (2, 0, True, 3, {}),
-         (2, 4, True, 3, {}),
-         (4, 0, True, 2, {}),
-         (4, 3, True, 2, {}),
-         (4, 6, True, 2, {}),
-         (6, 0, True, 1, {}),
-         (6, 2, True, 1, {}),
-         (6, 4, True, 1, {}),
-         (6, 6, True, 1, {})]
-    coords = [(1, 3), (1, 4), (1, 5), (1, 6), (1,7),
-              (2, 3), (2, 4), (2, 5), (2, 6), (2, 7),
-              (3, 3), (3, 4), (3, 5), (3, 6), (3, 7)]
-    for coord in coords:
-        assert is_open_sea(coord[0], coord[1], f)==False
+    #diagonally adjacent square
+    f = [(6, 0, False, 4, {}),
+         (1, 0, False, 2, {}),
+         (7, 2, True, 1, {}),
+         (1, 3, True, 1, {}),
+         (5, 3, True, 3, {}),
+         (7, 4, True, 3, {}),
+         (6, 1, False, 2, {}),
+         (4, 7, True, 1, {}),
+         (9, 8, True, 1, {}),
+         (0, 9, False, 2, {})]
+    assert is_open_sea(2, 8, f)==False
 
 def test_is_open_sea5():
+    #Occupied square (not top left)
     f = [(6, 0, False, 4, {}),
-         (7, 2, True, 3, {}),
-         (3, 2, True, 3, {}),
-         (8, 4, True, 2, {}),
-         (5, 4, True, 2, {}),
-         (2, 4, True, 2, {}),
-         (2, 9, True, 1, {}),
-         (6, 7, True, 1, {}),
-         (6, 5, True, 1, {}),
-         (6, 3, True, 1, {})]
-    coords = [(2, 1), (2, 2), (2, 3),
-              (3, 1), (3, 2), (3, 3),
-              (4, 1), (4, 2), (4, 3),
-              (5, 1), (5, 2), (5, 3),
-              (6, 1), (6, 2), (6, 3)]
-    for coord in coords:
-        assert is_open_sea(coord[0], coord[1], f)==False
+         (1, 0, False, 2, {}),
+         (7, 2, True, 1, {}),
+         (1, 3, True, 1, {}),
+         (5, 3, True, 3, {}),
+         (7, 4, True, 3, {}),
+         (6, 1, False, 2, {}),
+         (4, 7, True, 1, {}),
+         (9, 8, True, 1, {}),
+         (0, 9, False, 2, {})]
+    assert is_open_sea(9, 0, f)==False
 
 def test_ok_to_place_ship_at1():
     #no overlap, no adjacent squares
