a4e8c45c7d2a6b7ad50a84ff6de12729af57b33e
Wong-YK <59433231+Wong-YK@users.noreply.github.com>
2020-12-19 10:20:18 +0000

    wrote test_update_board_sink1() and test_update_board_sink2()

--- test_battleships.py
+++ test_battleships.py
@@ -370,3 +370,20 @@ def test_update_board_miss1():
     updated = update_board_miss(1, 3, board)
     assert updated[1][3]=="-"
 
+def test_update_board_sink1():
+    #sink a cruiser
+    board = [[".", ".", ".", ".", ".", ".", ".", ".", ".", "."] for i in range(10)]
+    ship_sunk = (1, 0, True, 3, {(1, 0), (1, 1), (1, 2)})
+    update_board_sink(ship_sunk, board)
+    assert board[1][0]=="C" and board[1][1]=="C" and board[1][2]=="C"
+
+def test_update_board_sink2():
+    #sink a submarine
+    board = [[".", ".", ".", ".", ".", ".", ".", ".", ".", "."] for i in range(10)]
+    ship_sunk = (5, 4, True, 1, {(5, 4)})
+    update_board_sink(ship_sunk, board)
+    assert board[5][4]=="S"
+
+
+
+
