0f6aec38629de87d6e89f320c1066c0ce3d2a707
Wong-YK <59433231+Wong-YK@users.noreply.github.com>
2020-12-28 10:19:58 +0000

    wrote test_is_open_sea6()

--- test_battleships.py
+++ test_battleships.py
@@ -55,85 +55,100 @@ def test_ship_type5():
 
 
 def test_is_open_sea1():
-    f = [(0, 0, True, 4, {}),
-         (2, 0, True, 3, {}),
-         (2, 4, True, 3, {}),
-         (4, 0, True, 2, {}),
-         (4, 3, True, 2, {}),
-         (4, 6, True, 2, {}),
-         (6, 0, True, 1, {}),
-         (6, 2, True, 1, {}),
-         (6, 4, True, 1, {}),
-         (6, 6, True, 1, {})]
+    #free square
+    f = [(0, 0, True, 4, set()),
+         (2, 0, True, 3, set()),
+         (2, 4, True, 3, set()),
+         (4, 0, True, 2, set()),
+         (4, 3, True, 2, set()),
+         (4, 6, True, 2, set()),
+         (6, 0, True, 1, set()),
+         (6, 2, True, 1, set()),
+         (6, 4, True, 1, set()),
+         (6, 6, True, 1, set())]
     assert is_open_sea(6, 6, f)==False
 
 def test_is_open_sea2():
     #occupied square (top left square of ship)
-    f = [(0, 0, True, 4, {}),
-         (2, 0, True, 3, {}),
-         (2, 4, True, 3, {}),
-         (4, 0, True, 2, {}),
-         (4, 3, True, 2, {}),
-         (4, 6, True, 2, {}),
-         (6, 0, True, 1, {}),
-         (6, 2, True, 1, {}),
-         (6, 4, True, 1, {}),
-         (6, 6, True, 1, {})]
+    f = [(0, 0, True, 4, set()),
+         (2, 0, True, 3, set()),
+         (2, 4, True, 3, set()),
+         (4, 0, True, 2, set()),
+         (4, 3, True, 2, set()),
+         (4, 6, True, 2, set()),
+         (6, 0, True, 1, set()),
+         (6, 2, True, 1, set()),
+         (6, 4, True, 1, set()),
+         (6, 6, True, 1, set())]
     assert is_open_sea(4, 0, f)==False
 
 def test_is_open_sea3():
     #vertically adjacent square
-    f = [(6, 0, False, 4, {}),
-         (1, 0, False, 2, {}),
-         (7, 2, True, 1, {}),
-         (1, 3, True, 1, {}),
-         (5, 3, True, 3, {}),
-         (7, 4, True, 3, {}),
-         (6, 1, False, 2, {}),
-         (4, 7, True, 1, {}),
-         (9, 8, True, 1, {}),
-         (0, 9, False, 2, {})]
+    f = [(6, 0, False, 4, set()),
+         (1, 0, False, 2, set()),
+         (7, 2, True, 1, set()),
+         (1, 3, True, 1, set()),
+         (5, 3, True, 3, set()),
+         (7, 4, True, 3, set()),
+         (6, 8, False, 2, set()),
+         (4, 7, True, 1, set()),
+         (9, 8, True, 1, set()),
+         (0, 9, False, 2, set())]
     assert is_open_sea(3, 0, f)==False
 
 def test_is_open_sea4():
     #diagonally adjacent square
-    f = [(6, 0, False, 4, {}),
-         (1, 0, False, 2, {}),
-         (7, 2, True, 1, {}),
-         (1, 3, True, 1, {}),
-         (5, 3, True, 3, {}),
-         (7, 4, True, 3, {}),
-         (6, 1, False, 2, {}),
-         (4, 7, True, 1, {}),
-         (9, 8, True, 1, {}),
-         (0, 9, False, 2, {})]
+    f = [(6, 0, False, 4, set()),
+         (1, 0, False, 2, set()),
+         (7, 2, True, 1, set()),
+         (1, 3, True, 1, set()),
+         (5, 3, True, 3, set()),
+         (7, 4, True, 3, set()),
+         (6, 8, False, 2, set()),
+         (4, 7, True, 1, set()),
+         (9, 8, True, 1, set()),
+         (0, 9, False, 2, set())]
     assert is_open_sea(2, 8, f)==False
 
 def test_is_open_sea5():
     #Occupied square (not top left)
-    f = [(6, 0, False, 4, {}),
-         (1, 0, False, 2, {}),
-         (7, 2, True, 1, {}),
-         (1, 3, True, 1, {}),
-         (5, 3, True, 3, {}),
-         (7, 4, True, 3, {}),
-         (6, 1, False, 2, {}),
-         (4, 7, True, 1, {}),
-         (9, 8, True, 1, {}),
-         (0, 9, False, 2, {})]
+    f = [(6, 0, False, 4, set()),
+         (1, 0, False, 2, set()),
+         (7, 2, True, 1, set()),
+         (1, 3, True, 1, set()),
+         (5, 3, True, 3, set()),
+         (7, 4, True, 3, set()),
+         (6, 8, False, 2, set()),
+         (4, 7, True, 1, set()),
+         (9, 8, True, 1, set()),
+         (0, 9, False, 2, set())]
     assert is_open_sea(9, 0, f)==False
+
+def test_is_open_sea6():
+    #horizontally adjacent square
+    f = [(6, 0, False, 4, set()),
+         (1, 0, False, 2, set()),
+         (7, 2, True, 1, set()),
+         (1, 3, True, 1, set()),
+         (5, 3, True, 3, set()),
+         (7, 4, True, 3, set()),
+         (6, 8, False, 2, set()),
+         (4, 7, True, 1, set()),
+         (9, 8, True, 1, set()),
+         (0, 9, False, 2, set())]
+    assert is_open_sea(2, 1, f)==False
 
 def test_ok_to_place_ship_at1():
     #no overlap, no adjacent squares
-    f = [(0, 0, True, 4, {}),
-         (2, 0, True, 3, {}),
-         (2, 4, True, 3, {}),
-         (4, 0, True, 2, {}),
-         (4, 3, True, 2, {}),
-         (4, 6, True, 2, {}),
-         (6, 0, True, 1, {}),
-         (6, 2, True, 1, {}),
-         (6, 4, True, 1, {})]
+    f = [(0, 0, True, 4, set()),
+         (2, 0, True, 3, set()),
+         (2, 4, True, 3, set()),
+         (4, 0, True, 2, set()),
+         (4, 3, True, 2, set()),
+         (4, 6, True, 2, set()),
+         (6, 0, True, 1, set()),
+         (6, 2, True, 1, set()),
+         (6, 4, True, 1, set())]
     assert ok_to_place_ship_at(6, 6, True, 1, f)==True
 
 def test_ok_to_place_ship_at2():
